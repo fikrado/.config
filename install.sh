4fde9ee25e3f7c76c932977ff7ab6bc403a9d550
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2022-08-23 14:22:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo su
 sleep(10)
-apt-get install vlc vlc-plugin-bittorrent vim kitty mpv stacer python3 python3-pip plank conky* --fix-missing --fix-broken -y
+apt-get install vlc vlc-plugin-bittorrent vim kitty mpv stacer python3 python3-pip scrcpy plank conky* --fix-missing --fix-broken -y
 sleep(5)
 unzip icons.zip && mv icons /usr/share/icons 
 sleep(5)
